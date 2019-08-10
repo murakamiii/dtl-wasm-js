6eb8c16ac03f7fcab2d98da796e6bfc039480d90
test <9khc727ru7@gmail.com>
2019-08-10 18:12:46 +0900

    only hello world

---
+++ src/bind.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+// #include <DTL/DTL.hpp>
+
+int main() {
+		std::cout << "hi" << "\n";
+		
+		// using shape_t = std::uint_fast8_t;
+		// std::array<std::array<shape_t, 48>, 36> matrix{ {} };
+		// auto succeed = dtl::shape::SimpleRogueLike<shape_t>(1, 2, 3, 4, 5, 2, 5, 2).draw(matrix);
+		// std::cout << std::boolalpha << succeed << "\n";
+}
