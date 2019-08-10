116d5b8985a2cee19796738eb8bd35dc2b3b4936
test <9khc727ru7@gmail.com>
2019-08-10 19:16:45 +0900

    とりあえずwandboxから取ってくる

--- src/bind.cpp
+++ src/bind.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <iomanip>
 
-// #include <DTL/DTL.hpp>
+// #include <DTL.hpp>
+#include "../dtlwandbox/DTL.hpp"
 
 int main() {
 		std::cout << "hi" << "\n";
 		
-		// using shape_t = std::uint_fast8_t;
-		// std::array<std::array<shape_t, 48>, 36> matrix{ {} };
-		// auto succeed = dtl::shape::SimpleRogueLike<shape_t>(1, 2, 3, 4, 5, 2, 5, 2).draw(matrix);
-		// std::cout << std::boolalpha << succeed << "\n";
+		using shape_t = std::uint_fast8_t;
+
+		std::array<std::array<shape_t, 48>, 36> matrix{ {} };
+		auto succeed = dtl::shape::SimpleRogueLike<shape_t>(1, 2, 3, 4, 5, 2, 5, 2).draw(matrix);
+		std::cout << std::boolalpha << succeed << "\n";
+		dtl::console::OutputString<shape_t>("//", "##", "%%").draw(matrix);
 }
