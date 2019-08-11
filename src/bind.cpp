ad76c7a364d718e48c8b02ff11a3be80fb02ef54
test <9khc727ru7@gmail.com>
2019-08-11 10:07:26 +0900

    仮実装

--- src/bind.cpp
+++ src/bind.cpp
@@ -21,16 +21,16 @@ int plusOne(int number) {
 	return number + 1;
 }
 
-std::vector<std::vector<std::uint_fast8_t>>getSimpleRogueLike() {
+std::vector<std::vector<int>>getSimpleRogueLike() {
 	using shape_t = std::uint_fast8_t;
 
 	std::array<std::array<shape_t, 48>, 36> matrix{ {} };
 	dtl::shape::SimpleRogueLike<shape_t>(1, 2, 3, 4, 5, 2, 5, 2).draw(matrix);
 
-	std::vector<std::vector<shape_t>> vec;
+	std::vector<std::vector<int>> vec;
 	for (auto &&row : matrix)
 	{
-		vec.push_back(std::vector<shape_t>(std::begin(row), std::end(row)));
+		vec.push_back(std::vector<int>(std::begin(row), std::end(row)));
 	}
 	return vec;
 }
@@ -39,6 +39,6 @@ EMSCRIPTEN_BINDINGS(my_module) {
 	emscripten::function("plusOne", &plusOne);
 	emscripten::function("getSimpleRogueLike", &getSimpleRogueLike);
 
-	emscripten::register_vector<std::vector<std::uint_fast8_t>>("std::vector<std::vector<std::uint_fast8_t>>");
-	emscripten::register_vector<std::uint_fast8_t>("std::vector<std::uint_fast8_t>");
+	emscripten::register_vector<int>("vector<int>");
+	emscripten::register_vector<std::vector<int>>("vector<vector<int>>");
 }
