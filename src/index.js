b00bc75412bc7b275b3766e394d4955c62d01c60
test <9khc727ru7@gmail.com>
2019-08-12 14:38:28 +0900

    src/index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const mod = require('wasm/dtl')
+
+const modulePromise = new Promise(resolve =>
+  mod.onRuntimeInitialized = _ => {
+    resolve(mod);
+  }
+)
+
+module.exports = modulePromise
+module.exports.default = modulePromise
